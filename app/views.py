f1dd57d6633c440de13dd75e3886e3b2dadc1c5f
Erasmo Montes <erasmomontes07@gmail.com>
2017-04-15 15:05:28 -0400

    Added: New content variables.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
+""" This containt all view of app. """
+
 from django.shortcuts import render_to_response
+from . import models
 
 
 def home(request):
-    return render_to_response('index.html')
+    """ Principal view. """
+    context = {"rentables": models.Rentable.objects.all(),
+               "prices": models.Prices.objects.all(),
+               "allience": models.Allience.objects.first(),
+               "feature": models.Feature.objects.first(),
+               "sections": models.Section.objects.all()}
+    return render_to_response('index.html', context)

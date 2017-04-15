9cda51c0975b46495e35da66b81f94677ee6ad4f
Erasmo Montes <erasmomontes07@gmail.com>
2017-04-15 15:04:07 -0400

    Added: New models manage visual content.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Percent)
+admin.site.register(models.Rentable)
+admin.site.register(models.Prices)
+admin.site.register(models.Caracterist)
+admin.site.register(models.Allience)
+admin.site.register(models.Section)
+admin.site.register(models.Feature)
